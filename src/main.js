d9861a77e7d6acf97171431dbc434a061b3053aa
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2024-10-08 14:59:24 -0300

    fix: production mode

--- src/main.js
+++ src/main.js
@@ -1,22 +1,37 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const childProcess = require('child_process');
-
+const net = require('net');
+const isDev = process.env.NODE_ENV === 'development';
 
 let mainWindow = null;
 
 function startBackend() {
   console.log('Iniciando o backend Express...');
 
-  const serverProcess = childProcess.spawn('npm', ['run', 'dev'], {
+  const buildProcess = childProcess.spawn('npm', ['run', 'build'], {
     cwd: path.join(__dirname, '../server'),
     stdio: 'inherit',
   });
+
+  const serverProcess = childProcess.spawn('npm', ['run', isDev ? 'dev' : 'start'], {
+    cwd: path.join(__dirname, '../server'),
+    stdio: 'inherit',
+  });
+
+  if (!buildProcess) {
+    console.error('Erro ao tentar iniciar o processo de build do backend.');
+    return;
+  }
 
   if (!serverProcess) {
     console.error('Erro ao tentar iniciar o processo do backend.');
     return;
   }
+
+  buildProcess.on('error', (err) => {
+    console.error('Erro ao iniciar o build do backend:', err);
+  });
 
   serverProcess.on('error', (err) => {
     console.error('Erro ao iniciar o backend:', err);
@@ -29,34 +44,93 @@ function startBackend() {
   console.log('Processo do backend iniciado com sucesso.');
 }
 
-async function createWindow() {
-  const { default: isDev } = await import('electron-is-dev');
+function waitForReactDevServer(port, timeout = 30000) {
+  return new Promise((resolve, reject) => {
+    const start = Date.now();
+    const interval = setInterval(() => {
+      const client = new net.Socket();
 
-  mainWindow = new BrowserWindow({
-    width: 1280,
-    height: 800,
-    webPreferences: {
-      nodeIntegration: false,
-      contextIsolation: true,
-      disableHardwareAcceleration: true, 
-      enableBlinkFeatures: 'None',
-    },
-    useVsync: false,
+      client.connect({ port }, () => {
+        clearInterval(interval);
+        client.end();
+        resolve(true);
+      });
+
+      client.on('error', () => {
+        if (Date.now() - start >= timeout) {
+          clearInterval(interval);
+          reject(new Error('Dev server timeout'));
+        }
+      });
+    }, 1000);
   });
+}
 
-  const startURL = isDev
-    ? 'http://localhost:3000'
-    : `file://${path.join(__dirname, '../build/index.html')}`;
+async function createWindow() {
+  if (isDev) {
+    console.log('Iniciando o processo de desenvolvimento do frontend...');
 
-  mainWindow.loadURL(startURL);
+    const devFrontProcess = childProcess.spawn('npm', ['run', 'start'], {
+      cwd: path.join(__dirname, '../client'),
+      stdio: 'inherit',
+    });
 
-  mainWindow.openDevTools();
+    if (!devFrontProcess) {
+      console.error('Erro ao tentar iniciar o processo de build dev do frontend.');
+      return;
+    }
 
-  mainWindow.on('closed', () => (mainWindow = null));
+    devFrontProcess.on('error', (err) => {
+      console.error('Erro ao iniciar o build dev do frontend:', err);
+    });
+
+    try {
+      await waitForReactDevServer(3000);
+      console.log('Frontend está pronto, criando a janela do Electron...');
+
+      mainWindow = new BrowserWindow({
+        width: 1280,
+        height: 800,
+        webPreferences: {
+          nodeIntegration: false,
+          contextIsolation: true,
+          disableHardwareAcceleration: true, 
+          enableBlinkFeatures: 'None',
+        },
+        useVsync: false,
+      });
+
+      mainWindow.loadURL('http://localhost:3000');
+
+      mainWindow.on('closed', () => (mainWindow = null));
+    } catch (error) {
+      console.error('Erro ao iniciar o frontend:', error);
+    }
+  } else {
+    console.log('Iniciando o frontend em modo produção...');
+
+    mainWindow = new BrowserWindow({
+      width: 1280,
+      height: 800,
+      webPreferences: {
+        nodeIntegration: false,
+        contextIsolation: true,
+        disableHardwareAcceleration: true, 
+        enableBlinkFeatures: 'None',
+      },
+      useVsync: false,
+    });
+
+    const startURL = `file://${path.join(__dirname, '../client/build/index.html')}`;
+
+    mainWindow.loadURL(startURL);
+
+    mainWindow.on('closed', () => (mainWindow = null));
+  }
 }
 
 app.on('ready', () => {
-  // startBackend();
+  startBackend();
   createWindow();
 });
 
