6e05d9e3c79e10a7ea99eb735dc4330b76be5dfa
Alexandre Machado <80935784@telefonicati.onmicrosoft.com>
2024-10-09 13:54:38 -0300

    feat: add mistica theme provider

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,19 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  ThemeContextProvider,
+  getVivoNewSkin,
+  ColorScheme,
+  Locale,
+  RegionCode,
+} from "@telefonica/mistica";
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const misticaTheme = {
+  skin: getVivoNewSkin(),
+  colorScheme: "light" as ColorScheme,
+  i18n: {
+    locale: "pt-BR" as Locale,
+    phoneNumberFormattingRegionCode: "pt" as RegionCode,
+  },
+};
+
+root.render(
+  <ThemeContextProvider theme={misticaTheme}>
+    <App />
+  </ThemeContextProvider>
+);
