aa4d6074d54e91c004f6a9fe2f0b6accd2e9ff0b
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2024-10-11 14:56:54 -0300

    test: test build software

--- src/main.js
+++ src/main.js
@@ -4,8 +4,6 @@ const childProcess = require('child_process');
 const net = require('net');
 require('dotenv').config();
 
-const isDev = process.env.NODE_ENV === 'development';
-
 let mainWindow = null;
 let serverProcess = null;
 let devFrontProcess = null;
@@ -13,15 +11,11 @@ let devFrontProcess = null;
 function startBackend() {
   console.log('Iniciando o backend Express...');
 
-  const serverPath = isDev
-    ? path.join(__dirname, '../server')
-    : path.join(process.resourcesPath, 'server');
-
-  const nodeExecutable = isDev ? 'node' : process.execPath;
+  const serverPath = path.join((process.resourcesPath, '../server'));
 
   serverProcess = childProcess.spawn(
-    nodeExecutable, 
-    isDev ? ['run', 'dev'] : [path.join(serverPath, 'server.js')], 
+    'node', 
+    [path.join(serverPath, 'server.js')], 
     {
       cwd: serverPath,
       stdio: 'inherit',
@@ -47,68 +41,9 @@ function stopBackend() {
   }
 }
 
-function waitForReactDevServer(port, timeout = 30000) {
-  return new Promise((resolve, reject) => {
-    const start = Date.now();
-    const interval = setInterval(() => {
-      const client = new net.Socket();
-
-      client.connect({ port }, () => {
-        clearInterval(interval);
-        client.end();
-        resolve(true);
-      });
-
-      client.on('error', () => {
-        if (Date.now() - start >= timeout) {
-          clearInterval(interval);
-          reject(new Error('Dev server timeout'));
-        }
-      });
-    }, 1000);
-  });
-}
-
 async function createWindow() {
   console.log('Iniciando a janela do Electron...');
 
-  if (isDev) {
-    console.log('Iniciando o processo de desenvolvimento do frontend...');
-
-    const clientPath = path.join(__dirname, '../client');
-
-    devFrontProcess = childProcess.spawn('npm', ['run', 'start'], {
-      cwd: clientPath,
-      stdio: 'inherit',
-    });
-
-    devFrontProcess.on('error', (err) => {
-      console.error('Erro ao iniciar o build dev do frontend:', err);
-    });
-
-    try {
-      await waitForReactDevServer(3000);
-      console.log('Frontend está pronto, criando a janela do Electron...');
-
-      mainWindow = new BrowserWindow({
-        width: 1280,
-        height: 800,
-        webPreferences: {
-          nodeIntegration: false,
-          contextIsolation: true,
-          disableHardwareAcceleration: true,
-          enableBlinkFeatures: 'None',
-        },
-        useVsync: false,
-      });
-
-      mainWindow.loadURL('http://localhost:3000');
-
-      mainWindow.on('closed', () => (mainWindow = null));
-    } catch (error) {
-      console.error('Erro ao iniciar o frontend:', error);
-    }
-  } else {
     console.log('Iniciando o frontend em modo produção...');
 
     mainWindow = new BrowserWindow({
@@ -129,7 +64,7 @@ async function createWindow() {
 
     mainWindow.on('closed', () => (mainWindow = null));
   }
-}
+
 
 function stopFrontend() {
   if (devFrontProcess) {
@@ -140,7 +75,7 @@ function stopFrontend() {
 }
 
 app.on('ready', () => {
-  startBackend();
+  // startBackend();
   createWindow();
 });
 
