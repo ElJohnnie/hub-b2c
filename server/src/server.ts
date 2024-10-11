151463e6d1d3aaec83cda94f181a92ac57354b58
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2024-10-11 20:21:08 -0300

    first commit

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,12 +2,16 @@
 import express from 'express';
 import cors from 'cors';
 import commandRoutes from './routes/commandRoutes';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 2345;
 
 app.use(cors());
 app.use(express.json());
+
+// servir arquivos estáticos aqui no server e tratar o server diretamente como um só
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', commandRoutes);
 
