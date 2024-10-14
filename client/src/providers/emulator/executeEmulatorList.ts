6902217b43c276699b259d640a7485bf516bc012
Alexandre Machado <80935784@telefonicati.onmicrosoft.com>
2024-10-14 10:07:50 -0300

    chore: change gateway path at emulator list

--- client/src/providers/emulator/executeEmulatorList.ts
+++ client/src/providers/emulator/executeEmulatorList.ts
@@ -1,4 +1,4 @@
-import { CommandGateway } from "../gateways/commandGateway";
+import { CommandGateway } from "../../gateways/commandGateway";
 
 export class ExecuteEmulatorListProvider {
   private commandGateway: CommandGateway;
