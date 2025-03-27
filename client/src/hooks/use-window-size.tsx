48e259f2ec8eaa39bdfce65e9cd2772231622eeb
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2025-03-26 23:22:06 -0300

    fix: eslint and resize

--- client/src/hooks/use-window-size.tsx
+++ client/src/hooks/use-window-size.tsx
@@ -1,31 +1,31 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export default function useWindowDimensions() {
   const hasWindow = typeof window !== "undefined";
 
-  function getWindowDimensions() {
+  const getWindowDimensions = useCallback(() => {
     const width = hasWindow ? window.innerWidth : null;
     const height = hasWindow ? window.innerHeight : null;
     return {
       width,
       height,
     };
-  }
+  }, [hasWindow]);
 
   const [windowDimensions, setWindowDimensions] = useState(
     getWindowDimensions()
   );
 
-  function handleResize() {
+  const handleResize = useCallback(() => {
     setWindowDimensions(getWindowDimensions());
-  }
-  
+  }, [getWindowDimensions]);
+
   useEffect(() => {
     if (hasWindow) {
       window.addEventListener("resize", handleResize);
       return () => window.removeEventListener("resize", handleResize);
     }
-  }, [hasWindow]);
+  }, [hasWindow, handleResize]);
 
   return windowDimensions;
 }
